cbdbf8d98830e8454dbf21b81d4b5e037751ff44
Juan Mata <jjmarrue@gmail.com>
2022-08-03 13:28:23 -0400

    Add script to test onclick event on grid

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const customBtn = document.querySelector('#custom-grid');
 const resetBtn = document.querySelector('#reset');
 const grid = document.querySelector('.grid-container');
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let cellArray = document.getElementsByClassName('cell');
 
 customBtn.addEventListener('click', () => {
   let userGridSize = prompt('Enter a grid size between 1- 100: ');
@@ -15,22 +17,57 @@ resetBtn.addEventListener('click', () => {
     let cell = document.createElement('div');
     cell.style.flexBasis = '6.25%';
     cell.className = 'cell';
-    grid.appendChild(cell);   
-    console.log(i);
+    grid.appendChild(cell);
   } 
 });
 
 //256 cells and 6.25% flexBasis are default values for 16x16 grid
 function addCells(numberOfCells=256, flexBasisVal='6.25%'){
   grid.innerHTML = '';
+  let cell = '';
   for (let i = 1; i <= numberOfCells; i++){
-    let cell = document.createElement('div');
+    cell = document.createElement('div');
     cell.style.flexBasis = flexBasisVal;
     cell.className = 'cell';
-    grid.appendChild(cell);   
-    console.log(i);
-  } 
+    grid.appendChild(cell);
+  }
+
+ //console.log(cellArray.item(15));
+
+
+
+
+
+ for (let j = 0; j < cellArray.length; j++) {
+  cellArray.item(j).addEventListener('click', () => {
+    cellArray.item(j).style.backgroundColor = 'red';
+  });
+  }
 }
+
+
+
+
+
+
+// grid.array.forEach(element => {
+//   console.log(element);
+// });
+
+// grid.addEventListener('click', function(){
+
+//   console.log(typeof(gridCell)); 
+//   // let hexColor = '#';
+//   // for (let i = 0; i < 6; i++){
+//   //   hexColor += hex[getRandomNumber()];
+//   // }
+//   // gridCell.style.backgroundColor = hexColor;
+// });
+
+function getRandomNumber(){
+  return Math.floor(Math.random()*hex.length);
+}
+
 
 addCells();
 
