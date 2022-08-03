d075f375f6c4b47fe767f550b6a9d2071b009270
Juan Mata <jjmarrue@gmail.com>
2022-08-03 11:06:22 -0400

    Implement code for re-sizable grid
    Add eventListeners to generate custom grid and reset grid so that
    users can enter a grid size.

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-// create a row of 4 squares
-const square = document.createElement('div');
+const customBtn = document.querySelector('#custom-grid');
+const resetBtn = document.querySelector('#reset');
 const grid = document.querySelector('.grid-container');
-let divs = '';
 
-function addCells(){
-  for (let i = 1; i <= 100; i++){
+customBtn.addEventListener('click', () => {
+  let userGridSize = prompt('Enter a grid size between 1- 100: ');
+  let numOfCells = Math.pow(userGridSize,2);
+  let flexBasisVal = 100 / userGridSize;
+  addCells(numOfCells, `${flexBasisVal}%`);
+});
 
-    divs += '<div class="cell"></div>';
-  }
-  grid.innerHTML = divs;
+resetBtn.addEventListener('click', () => {
+  grid.innerHTML = '';
+  for (let i = 1; i <= 256; i++){
+    let cell = document.createElement('div');
+    cell.style.flexBasis = '6.25%';
+    cell.className = 'cell';
+    grid.appendChild(cell);   
+    console.log(i);
+  } 
+});
+
+//256 cells and 6.25% flexBasis are default values for 16x16 grid
+function addCells(numberOfCells=256, flexBasisVal='6.25%'){
+  grid.innerHTML = '';
+  for (let i = 1; i <= numberOfCells; i++){
+    let cell = document.createElement('div');
+    cell.style.flexBasis = flexBasisVal;
+    cell.className = 'cell';
+    grid.appendChild(cell);   
+    console.log(i);
+  } 
 }
 
 addCells();
+
