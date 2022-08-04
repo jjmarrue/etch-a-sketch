d0bd7e5ad44e735dd4f68c3a035bc597291e835a
Juan Mata <jjmarrue@gmail.com>
2022-08-04 11:17:09 -0400

    Complete functionality for random color generator

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
+// Access DOM nodes
+
 const customBtn = document.querySelector('#custom-grid');
 const resetBtn = document.querySelector('#reset');
 const grid = document.querySelector('.grid-container');
 let cellArray = document.getElementsByClassName('cell');
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-// function getRandomNumber(){
-//   return Math.floor(Math.random()*hex.length);
-// }
+// Add event listeners
+
+resetBtn.addEventListener('click', ()=>addCells(256,'6.25%'));
+customBtn.addEventListener('click', createCustomGrid);
+
+// Set grid size based on user input
+
+function createCustomGrid(e){
+  let userGridSize = prompt('Enter a grid size between 1- 100: ');
+  let numOfCells = Math.pow(userGridSize,2);
+  let flexBasisVal = 100 / userGridSize;
+  addCells(numOfCells, `${flexBasisVal}%`);
+}
+
+// Create random number for RGB color
 
 function getRandomNumber(){
   return Math.floor(Math.random()*256);
 }
 
-customBtn.addEventListener('click', () => {
-  let userGridSize = prompt('Enter a grid size between 1- 100: ');
-  let numOfCells = Math.pow(userGridSize,2);
-  let flexBasisVal = 100 / userGridSize;
-  addCells(numOfCells, `${flexBasisVal}%`);
-});
+// Create grid - default parameters are for 16x16 grid
 
-resetBtn.addEventListener('click', () => {
-  grid.innerHTML = '';
-  for (let i = 1; i <= 256; i++){
-    let cell = document.createElement('div');
-    cell.style.flexBasis = '6.25%';
-    cell.className = 'cell';
-    grid.appendChild(cell);
-  } 
-});
-
-resetBtn.addEventListener('click', ()=>addCells(256,'6.25%'));
-
-// default arguments for 16x16 grid
 function addCells(numberOfCells=256, flexBasisVal='6.25%'){
+  let cell;
   grid.innerHTML = '';
-  let cell = '';
   for (let i = 1; i <= numberOfCells; i++){
     cell = document.createElement('div');
     cell.style.flexBasis = flexBasisVal;
@@ -42,18 +37,17 @@ function addCells(numberOfCells=256, flexBasisVal='6.25%'){
     grid.appendChild(cell);
   }
 
-  // Access each cell of the grid, and apply a random rgb color to the cell
+  // Access each cell in the grid and apply a random rgb color to that cell
+
   for (let j = 0; j < cellArray.length; j++) {
-    cellArray.item(j).addEventListener('mouseover', () => {
+    cellArray.item(j).addEventListener('mouseover', () => {   
       let rColor = getRandomNumber();
       let gColor = getRandomNumber();
       let bColor = getRandomNumber();
-      const darkenVal = 25.5;
-      cellArray.item(j).style.backgroundColor = `rgb(${rColor}, ${gColor}, ${bColor})`;
-      //cellArray.item(j).style.backgroundColor = (parseFloat(cellArray.item(j).style.opacity) || 0) + 0.1;
-    });
+      let rgbColor = `rgb(${rColor}, ${gColor}, ${bColor})`;
+      cellArray.item(j).style.backgroundColor = rgbColor;
+    });    
   }
 }
 
 addCells();
-
